0905f6af14357ee403dbea39757f5774f9790dde
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-05-04 16:30:59 -0400

    Loosen restrictions on arbitrary values passed to `getIdentVarValue`

--- utils.ts
+++ utils.ts
@@ -6,39 +6,13 @@ const validateVarName = (name: string) => {
   }
 }
 
-export const getIdentVarValue = (modifier: string, isV4: boolean, reserved: string[] = []) => {
+export const getIdentVarValue = (modifier: string) => {
   if (modifier.startsWith('--')) {
     validateVarName(modifier);
     return modifier;
   }
-  if (modifier.startsWith('(') && modifier.endsWith(')')) {
-    const modifierInner = modifier.slice(1, -1);
-    validateVarName(modifierInner);
-    if (!isV4) {
-      throw new Error(`This variable shorthand syntax is only supported in Tailwind CSS v4.0 and above: ${modifier}. In v3.x, you must use [${modifierInner}].`);
-    }
-    return `var(${modifierInner})`;
-  }
-  if (modifier.startsWith('[') && modifier.endsWith(']')) {
-    const modifierInner = modifier.slice(1, -1);
-    // reserved arbitrary value names
-    const reservedValues = [...reserved, 'inherit', 'initial', 'revert', 'unset'];
-    if (reservedValues.includes(modifierInner)) {
-      return modifierInner;
-    }
-    if (modifierInner.startsWith('var(--') && modifierInner.endsWith(')')) {
-      // validate the variable name inside `var(…)`
-      validateVarName(modifierInner.slice(4, -1));
-      return modifierInner;
-    }
-    if (modifierInner.startsWith('--')) {
-      validateVarName(modifierInner);
-      if (!isV4) {
-        return `var(${modifierInner})`;
-      }
-      return modifierInner;
-    }
-    throw new Error(`Invalid modifier: ${modifier}.`);
+  if (([['(',')'],['[',']']] as const).some(([start, end]) => modifier.startsWith(start) && modifier.endsWith(end))) {
+    return modifier;
   }
   return prefixAnchorName(modifier);
 }
